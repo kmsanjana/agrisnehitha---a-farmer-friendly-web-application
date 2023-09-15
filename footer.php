05314bea464bc73cc1189e6d8d94972d05462e51
Sanjana K M <145157101+kmsanjana@users.noreply.github.com>
2023-09-16 02:08:10 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,9 @@
+		<!-- Copyright -->
+		<div id="copyright" style="background-color:#e6d5cf;" >
+			<div class="container" style="color:#a06450;" >
+				<strong>Copyright @ <?php echo date(Y); ?> agri snehitha  - Designed by <a href="index.php">ISE Dept, SJBIT, Bangalore</a></strong>
+			</div>
+		</div>
+
+	</body>
+</html>
